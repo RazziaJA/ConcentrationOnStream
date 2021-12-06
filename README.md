5c5f35b8bbbe419ce0708debcb2669e788cd21cd
RazziaJA <13019628+RazziaJA@users.noreply.github.com>
2021-12-05 23:37:04 -0500

    Added app states section to readme. Added functions to generate a shuffled deck and lay it out into rows. Added components to render the cards with no interactivity.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+
+# Introduction
+
 A game of Concentration. The objective is to flip over cards two at a time seeking pairs of cards with the same rank and color.
+
+# App States
+
+* Game is over / not started:
+    * Can toggle between requiring a color match or just a rank match
+    * Can start a new game
+* Game is in progress:
+    Matched pairs will be face up at all times, unmatched cards will be face down unless they have been selected for the next pair.
+    * One card has been selected. It is face up and has a border highlight
+    * A second card has been selected.
+        * If it was a match, leave it visible and begin selecting the next guess (or end the game if that was the last pair)
+        * If it was not a match, wait some seconds, and then flip both guessed cards.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+import Card from "@heruka_urgyen/react-playing-cards/lib/TcB"
+
+const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+const suits = ["c", "d", "h", "s"]
+const totalCards = 52;
+
+/* Generates and shuffles an array containing 0..51 using a Schwartzian transform.
+ * Note: an inside-out Fisher-Yates shuffle would improve this from O(nlogn) to O(n). But I find this more readable and 52 isn't many.
+ * See https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ */
+function generateNewBoard() {
+  return [...Array(totalCards).keys()]
+    .map((value) => ({value, sort: Math.random()}))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({value}) => value);
+}
+
+function groupBoardIndicesIntoRows(cardsPerRow) {
+  var rows = [];
+  for (var i = 0; i < totalCards; ++i) {
+    if (i % cardsPerRow === 0) {
+      rows.push([]);
+    }
+    var row = Math.floor(i / cardsPerRow);
+    rows[row].push(i);
+  }
+  return rows;
+}
+
+function MemoryCard(props) {
+  return (
+    <Card card={ranks[props.cardNum % 13] + suits[Math.floor(props.cardNum / 13)]} height="100px" />
+  );
+}
+
+function Game(props) {
+  // eslint-disable-next-line
+  const [matched, setMatched] = useState(Array(totalCards).fill(false));
+
+  
+  return (
+    <div>
+      {props.rows.map((row, idx) =>
+        <ul key={idx}>
+          {row.map(cardIdx =>
+            <MemoryCard key={props.board[cardIdx]} cardNum={props.board[cardIdx]}/>
+            )}
+        </ul>)}
+    </div>
+  );
+}
 
 function App() {
+  // eslint-disable-next-line
+  const [board, setBoard] = useState(generateNewBoard());
+  // eslint-disable-next-line
+  const [rows, setRows] = useState(groupBoardIndicesIntoRows(13));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game rows={rows} board={board} />
     </div>
   );
 }
